ee9207e79882fcf9807dd5ac0ee2eace0c8a2fa7
CasperJ <c@sper.name>
2023-10-27 15:38:17 +0200

    returning IServiceCollection to support chaining

--- src/AzureDevops.Logger/ServiceCollectionExtensions.cs
+++ src/AzureDevops.Logger/ServiceCollectionExtensions.cs
@@ -9,9 +9,10 @@ public static class ServiceCollectionExtensions
     /// Registers the required services, so that <see cref="IAzDOLogger"/> can be injected and used in the application.
     /// </summary>
     /// <param name="serviceCollection"><see cref="IServiceCollection"/> instance.</param>
-    public static void AddAzureDevOpsLogger(this IServiceCollection serviceCollection)
+    public static IServiceCollection AddAzureDevOpsLogger(this IServiceCollection serviceCollection)
     {
         serviceCollection.AddSingleton<ILogMessageFactory, LogMessageFactory>();
         serviceCollection.AddSingleton<IAzDOLogger, AzDOLogger>();
+        return serviceCollection;
     }
 }
